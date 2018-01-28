0d80a798e4a17f33a668be029c424955fa645af1
Paul Du <du.paul136@gmail.com>
2018-01-28 01:05:16 +0100

    First implementation of LoadFile

--- LogParser.cpp
+++ LogParser.cpp
@@ -1,3 +1,4 @@
+#include "HTTPRequest.h"
 #include "LogParser.h"
 
 #include <iostream>
@@ -6,16 +7,141 @@
 #include <unordered_map>
 #include <map>
 #include <utility>
+#include <algorithm>
+#include <functional>
+
+namespace str
+{
+    static bool startsWith(const std::string &src, const std::string &prefix)
+    {
+        if (src.size() < prefix.size())
+            return false;
+
+        return std::equal(prefix.begin(), prefix.end(), src.begin());
+    }
+
+    static bool endsWith(const std::string &src, const std::string &suffix)
+    {
+        if (src.size() < suffix.size())
+            return false;
+
+        return std::equal(suffix.rbegin(), suffix.rend(), src.rbegin());
+    }
+}
+
+namespace url
+{
+    static std::string removePrefix(const std::string &url)
+    {
+        constexpr auto PREFIX = {"http://intranet-if.insa-lyon.fr",
+                                  "http://intranet-if",
+                                  "http://"};
+
+        for (const std::string &prefix : PREFIX)
+        {
+            if (str::startsWith(url, prefix))
+            {
+                //On part du premier / après le nom de domaine pour ignorer le numéro du port si présent
+                size_t startPos = url.find('/', prefix.size());
+                if (startPos != std::string::npos)
+                    return url.substr(startPos);
+                else
+                    return url.substr(prefix.size());
+            }
+        }
+
+        return url;
+    }
+
+    static std::string trim(const std::string &url)
+    {
+        constexpr auto DELIMITERS = {'&', '?', ';'};
+        size_t split = std::string::npos;
+        for (const auto &delim : DELIMITERS)
+            split = std::min(split, url.find(delim));
+
+        return url.substr(0, split);
+    }
+
+    static bool isExcluded(const std::string &url)
+    {
+        constexpr auto EXTENSIONS = {".bmp", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico", ".css", ".js"};
+        return std::any_of(EXTENSIONS.begin(), EXTENSIONS.end(), std::bind(str::endsWith, url, std::placeholders::_1));
+    }
+}
+
+namespace request
+{
+    static bool isAcceptedMethod(const std::string &method)
+    {
+        constexpr auto METHODS = {"GET", "POST"};
+        return std::any_of(METHODS.begin(), METHODS.end(), [method](const std::string &m) { return method == m; });
+    }
+
+    static bool isAcceptedCode(int code)
+    {
+        constexpr int HTTP_CODE_START = 200;
+        constexpr int HTTP_CODE_END = 399;
+        return code >= HTTP_CODE_START && code <= HTTP_CODE_END;
+    }
+
+    static int getHour(const std::string &dateTime)
+    {
+        //Exemple : [08/Sep/2012:11:16:02 +0200]
+        size_t pos = dateTime.find(':'); //Les deux caractères qui suivent sont l'heure
+        if (pos >= std::string::npos-2) //On teste dans ce sens pour éviter un overflow
+            return -1;
+
+        if (pos+2 >= dateTime.size())
+            return -1;
+
+        return (dateTime[pos+1]-'0')*10+(dateTime[pos+2]-'0');
+    }
+}
 
 LogParser::LogParser() : website(), top()
 {
 
 }
 
-bool LogParser::LoadFile(std::string filename, bool excludeImages,
+bool LogParser::LoadFile(std::string filename, bool exclude,
                          int selectHour, bool graph)
 {
-    return false;
+    std::ifstream file(filename);
+    if (file)
+    {
+        HTTPRequest request;
+        while (file >> request)
+        {
+            if (request::isAcceptedCode(request.HTTPCode()) &&
+                request::isAcceptedMethod(request.Method()))
+            {
+                if (selectHour < 0 || request::getHour(request.DateTime()) == selectHour)
+                {
+                    if (!exclude || !url::isExcluded(request.Document()))
+                        website[url::trim(request.Document())].referers[url::trim(url::removePrefix(request.Referer()))]++;
+                }
+            }
+        }
+
+        if (!file.eof())
+        {
+            std::cerr << "Erreur pendant la lecture du fichier " << filename << "." << std::endl;
+        }
+
+        computeTop();
+
+#ifdef DEBUG
+        std::cerr << website.size() << " documents chargés depuis " << filename << "." << std::endl;
+#endif
+    }
+    else
+    {
+        std::cerr << "Erreur lors de l'ouverture du fichier " << filename << "." << std::endl;
+        return false;
+    }
+
+    return true;
 }
 
 bool LogParser::GenerateDotFile(std::string filename)
