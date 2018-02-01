d207e909473df0a82893dcf6cb05a3bbbe5e0504
Paul Du <du.paul136@gmail.com>
2018-02-02 00:07:13 +0100

    Ajout de 8 tests

--- Tests/mktest.sh
+++ Tests/mktest.sh
@@ -1,3 +1,7 @@
+find . -name "*.dot" -delete
+
+./mkRandomLog.py > test.log
+
 echo "Test ID;Return code validation;Out result;StdErr result;File creation result;Global result" >results.csv
 nOk=0
 nKo=0
