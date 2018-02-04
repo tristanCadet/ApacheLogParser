2d8483464c219c2875c98f57085c20d39a28fed9
Paul Du <du.paul136@gmail.com>
2018-02-04 01:55:11 +0100

    Relance 3 fois les tests

--- Tests/mktest.sh
+++ Tests/mktest.sh
@@ -1,29 +1,40 @@
-find . -name "*.dot" -delete
-
-./mkRandomLog.py > test.log
-
-echo "Test ID;Return code validation;Out result;StdErr result;File creation result;Global result" >results.csv
 nOk=0
 nKo=0
 nTotal=0
 nMis=0
 
-for i in Test*
+for n in $(seq 3)
 do
-  ./test.sh $i results.csv
-  result=$?
-  if [ $result -eq 0 ]
+  find . -name "*.dot" -delete
+  ./mkRandomLog.py > test.log
+
+  echo "Test ID;Return code validation;Out result;StdErr result;File creation result;Global result" >results$n.csv
+
+  for i in Test*
+  do
+    ./test.sh "$i" results$n.csv
+    result=$?
+    if [ $result -eq 0 ]
+    then
+      let "nKo=$nKo+1"
+    elif [ $result -eq 1 ]
+    then
+      let "nOk=$nOk+1"
+    else
+      let "nMis=$nMis+1"
+    fi
+    let "nTotal=$nTotal+1"
+  done
+
+  if [ $nMis -gt 0 ]
   then
-    let "nKo=$nKo+1"
-  elif [ $result -eq 1 ]
-  then
-    let "nOk=$nOk+1"
-  else
-    let "nMis=$nMis+1"
+      echo "Error at run : $n"
+      break
   fi
-  let "nTotal=$nTotal+1"
 done
 
+echo "Number of runs   : $n"
+echo "-----------------------"
 echo "Passed tests     : $nOk"
 echo "Failed tests     : $nKo"
 echo "Misformed tests  : $nMis"
